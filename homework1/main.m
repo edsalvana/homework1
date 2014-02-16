6fca5d6ceaec38bde5261abc467b6455216b34c7
Ed Salvana <ed@visualgratis.com>
2014-02-15 18:49:31 -0800

    Initial Commit

---
+++ homework1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  homework1
+//
+//  Created by Ed Salvana on 2/15/14.
+//  Copyright (c) 2014 Ed Salvana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ homework1Tests/homework1Tests.m
@@ -0,0 +1,34 @@
+//
+//  homework1Tests.m
+//  homework1Tests
+//
+//  Created by Ed Salvana on 2/15/14.
+//  Copyright (c) 2014 Ed Salvana. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface homework1Tests : XCTestCase
+
+@end
+
+@implementation homework1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
